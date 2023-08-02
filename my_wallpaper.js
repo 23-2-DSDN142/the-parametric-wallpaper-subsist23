bee31a47bd5f8d5d3b85cc192f5f8cec90fdfb66
subsist23 <dancrichton@hotmail.com>
2023-08-03 09:26:00 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,12 +1,15 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
+let rect_width  = 100;
+let rect_height = 100;
+let ellipse_R = 50;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.output_mode(GLIDE_WALLPAPER);
+  //pWallpaper.output_mode(GRID_WALLPAPER);
+  //pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.resolution(NINE_PORTRAIT);
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -15,46 +18,69 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background("#235e48"); //dark green
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  noStroke()
-  fill(123)
-  rect(0,170,200,30)
+  
+//biglightgreenellipses
+noFill(0); 
+stroke(74, 255, 201)
+strokeWeight(.5)
+ellipse(100,100,ellipse_R*2);
+ellipse(100,100,ellipse_R/2+10);
+ellipse(100,100,ellipse_R/2+90);
 
-stroke(0,0,255)
-fill(0,0,255)
-ellipse(rectX+10,rectY-10,25) //bluelarge
+//darkgreenrects
+noFill();
+stroke(0,200,0);
+strokeWeight(1);
+rect (0,0,rect_width,rect_height);
+rect (10,10,rect_width,rect_height);
+rect (30,30,rect_width,rect_height);
 
-fill(0,0,255)
-ellipse(rectX+108,rectY+5,12) //blue
+//lightgreenrects
+noFill();
+stroke(120,230,120);
+strokeWeight(.5);
+rect (100,100,rect_width,rect_height);
+rect (90,90,rect_width,rect_height);
+rect (70,70,rect_width,rect_height);
 
-fill(0)  
-rect(rectX,rectY,rect_width,rect_height) //poles
+//greenellipses
+noFill();
+stroke(0,222,0);
+strokeWeight(.5);
+ellipse(150,150,ellipse_R);
+ellipse(50,50,ellipse_R);
+ellipse(100,100,ellipse_R);
+ellipse(150,50,ellipse_R);
+ellipse(50,150,ellipse_R);
 
-fill(0)  
-rect(rectX+100,rectY+10,rect_width-5,rect_height-30) //poles
-
-fill(120)
-rect(rectX,rectY,rect_width,rect_height-90) //greybox
-
-fill(120)
-rect(rectX+100,rectY+10,rect_width-5,rect_height-95) //greybox
-
-stroke(0,0,255)
+fill (74,255,201)
+stroke(74, 255, 201)
 strokeWeight(1)
-line(rectX+10,rect_height-10,rectY+10,rect_width+85) //firstline
+ellipse(150,50,ellipse_R/2);
+ellipse(50,150,ellipse_R/3);
 
-stroke(0,0,255)
-strokeWeight (1)
-line(rectY+10,rectX+105,200,90) //secondline
 
-stroke(0)
-fill(0)
-ellipse(rectX+108,rectY+4,6) //blackpart
-fill(0)
-ellipse(rectX+10,rectY-10,15) //bluepart
+stroke("#D4AF37"); //gold!
+strokeWeight(1)
+line (20,20,100,20);
+line (20,20,20,100);
+//line (20,20,100,100);
+line (20,20,20,100);
+strokeWeight(1)
+line (180,180,100,180);
+line (180,180,180,100);
+//line (180,180,100,100);
+line (170,170,110,170);
+line (170,170,170,110);
+
+
+
+
+
 
 
 }
