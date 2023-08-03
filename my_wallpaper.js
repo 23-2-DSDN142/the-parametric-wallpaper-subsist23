d1e2d525e34461cfef386003f2f167ad36d95209
subsist23 <dancrichton@hotmail.com>
2023-08-03 13:15:58 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,13 +1,13 @@
 //your parameter variables go here!
-let rect_width  = 100;
-let rect_height = 100;
+let rect_width  = 9;
+let rect_height = 24;
 let ellipse_R = 50;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(GLIDE_WALLPAPER);
+  //pWallpaper.output_mode(GLIDE_WALLPAPER);
   //pWallpaper.output_mode(GRID_WALLPAPER);
-  //pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(DEVELOP_GLYPH);
   pWallpaper.resolution(NINE_PORTRAIT);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
@@ -18,64 +18,174 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background("#235e48"); //dark green
+  background(255); 
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   
-//biglightgreenellipses
-noFill(0); 
-stroke(74, 255, 201)
-strokeWeight(.5)
-ellipse(100,100,ellipse_R*2);
-ellipse(100,100,ellipse_R/2+10);
-ellipse(100,100,ellipse_R/2+90);
-
-//darkgreenrects
-noFill();
-stroke(0,200,0);
+  //windscreen
+fill(100,100,255);
+stroke(0);
 strokeWeight(1);
-rect (0,0,rect_width,rect_height);
-rect (10,10,rect_width,rect_height);
-rect (30,30,rect_width,rect_height);
+ellipse(73,115,66,33)
 
-//lightgreenrects
-noFill();
-stroke(120,230,120);
-strokeWeight(.5);
-rect (100,100,rect_width,rect_height);
-rect (90,90,rect_width,rect_height);
-rect (70,70,rect_width,rect_height);
+  //round base
+fill(255,0,0);
+stroke(0);
+strokeWeight(1);
+ellipse(100,130,80,20)
 
-//greenellipses
-noFill();
-stroke(0,222,0);
-strokeWeight(.5);
-ellipse(150,150,ellipse_R);
-ellipse(50,50,ellipse_R);
-ellipse(100,100,ellipse_R);
-ellipse(150,50,ellipse_R);
-ellipse(50,150,ellipse_R);
+//connection between back wheel MIDDLE SHADING
+fill(180,0,0);
+stroke(0);
+strokeWeight(0)
+beginShape();
+vertex(131, 142); //bottomL
+vertex(130, 128); //topL
+vertex(80, 105); //topR
+vertex(81, 117); //bottomR
+endShape(CLOSE);
 
-fill (74,255,201)
-stroke(74, 255, 201)
+//connection between back wheel MIDDLE
+fill(255,0,0);
+stroke(0);
+strokeWeight(0.5)
+beginShape();
+vertex(130, 140); //bottomL
+vertex(130, 128); //topL
+vertex(80, 105); //topR
+vertex(80, 115); //bottomR
+endShape(CLOSE);
+
+
+//seat
+fill(0);
+rect(120,100,rect_width,rect_height-10)
+rect(95,110,rect_height+10,rect_width)
+
+//base background
+fill(255,0,0);
+stroke(0);
 strokeWeight(1)
-ellipse(150,50,ellipse_R/2);
-ellipse(50,150,ellipse_R/3);
+beginShape();
+vertex(20, 140); //bottomL
+vertex(40, 118); //topL
+vertex(100, 105); //topR
+vertex(100, 125); //bottomR
+endShape(CLOSE);
+
+//base segment
+fill(255,0,0);
+stroke(0);
+strokeWeight(0.5)
+beginShape();
+vertex(60, 130); //bottomL
+vertex(70, 118); //topL
+vertex(100, 105); //topR
+vertex(110, 115); //bottomR
+endShape(CLOSE);
+
+//base segment (DETAILING INSIDE)
+fill(255,0,0);
+stroke(0);
+strokeWeight(0.1)
+beginShape();
+vertex(66, 124); //bottomL
+vertex(76, 118); //topL
+vertex(103, 108); //topR
+vertex(107, 112); //bottomR
+endShape(CLOSE);
+
+//lil wheel holder thing
+stroke(0)
+strokeWeight(0.5)
+fill(255,0,0)
+ellipse(41,116,5,15)
+
+//front wheel  
+fill(23);
+noStroke();
+ellipse(30,130,39);
+
+//back wheel  
+fill(23);
+noStroke();
+ellipse(170,130,45);
 
 
-stroke("#D4AF37"); //gold!
-strokeWeight(1)
-line (20,20,100,20);
-line (20,20,20,100);
-//line (20,20,100,100);
-line (20,20,20,100);
-strokeWeight(1)
-line (180,180,100,180);
-line (180,180,180,100);
-//line (180,180,100,100);
-line (170,170,110,170);
-line (170,170,170,110);
+//connection between front wheel SHADING
+fill(180,0,0);
+stroke(0);
+strokeWeight(0)
+beginShape();
+vertex(31, 142); //bottomL
+vertex(30, 118); //topL
+vertex(80, 105); //topR
+vertex(80, 117); //bottomR
+endShape(CLOSE);
+
+//connection between front wheel
+fill(255,0,0);
+stroke(0);
+strokeWeight(0.5)
+beginShape();
+vertex(30, 140); //bottomL
+vertex(30, 118); //topL
+vertex(80, 105); //topR
+vertex(80, 115); //bottomR
+endShape(CLOSE);
+
+//inner front wheel SHADING
+fill(180,0,0);
+stroke(0);
+strokeWeight(0);
+ellipse(31,132,23)
+
+//inner front wheel
+fill(255,0,0);
+stroke(0);
+strokeWeight(0.5);
+ellipse(30,130,23)
+
+
+//inner back wheel SHADING
+fill(180,0,0);
+stroke(0);
+strokeWeight(0);
+ellipse(171,132,29)
+
+//connection between back wheel
+fill(180,0,0);
+stroke(0);
+strokeWeight(0)
+beginShape();
+vertex(131, 142); //bottomL
+vertex(130, 128); //topL
+vertex(170, 123); //topR
+vertex(171, 144); //bottomR
+endShape(CLOSE);
+
+//connection between back wheel
+fill(255,0,0);
+stroke(0);
+strokeWeight(0.5)
+beginShape();
+vertex(130, 140); //bottomL
+vertex(130, 128); //topL
+vertex(170, 123); //topR
+vertex(170, 142); //bottomR
+endShape(CLOSE);
+
+
+//inner back wheel
+fill(255,0,0);
+stroke(0);
+strokeWeight(0.5);
+ellipse(170,130,29)
+
+
+
+
 
 
 
